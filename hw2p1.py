64582e98f61ba1a622919caf42c591f77c3169de
Timur Kasimov <104742862+timurka43@users.noreply.github.com>
2024-03-01 10:56:30 -0600

    completed both gradient descent method and newton in multiple dimensions, tested on various inputs, graphed convergence rates for newton method

--- hw2p1.py
+++ hw2p1.py
@@ -2,25 +2,26 @@
 Title: Homework 2, part 1. Gradient Descent
 Author: Timur Kasimov (Grinnell, 2025)
 Data Created: February 20, 2024
-Date Updated: February 27, 2024
+Date Updated: March 1st, 2024
 
 Purpose: Implement and test a gradient descent method
 
 This is a homework assignment for my Computational Methods Class
 """
 
-import math
 import numpy as np
 
 
 MAXITER = 1000
 TOLERANCE = 1e-10
 
-A = np.array([1,0,3,2,2,2,0,1,2]).reshape(3, 3)
+# matrix and vector for part 2 (optimal step alpha)
+A = np.array([2,-1,0,-1,2,-1,0,-1,2]).reshape(3, 3)
 # print(A)
 b = np.array([2,0,2])
 # print(b)
 
+# constants for part 3 (backtracking method)
 RHO = 0.9
 c = 0.1
 
@@ -31,27 +32,29 @@ c = 0.1
 '''
 Pre-conditions:
   f: function of n variables
-  gradient: a function that computes the gradient of f at given coordinates
+  gradient df: a function that computes the gradient of f at given coordinates
   guess: vector of size n with initial guess coordinates
-  alpha: constant step size
+  alpha: a function that calculates optimal step, or a constant step size
+  backtracking: if True, use backtracking method instead of constant/optimal alpha
 Post-conditions: 
   returns vector of size n with a coordinates of the min of f
 '''
 def gradient_descent(f, df, guess, alpha, backtracking = False):
-    for i in range(1, MAXITER+1):
+    # iterative process, stopping condition: reached max iterations (MAXITER)
+    for i in range(0, MAXITER):
         # Check if the current guess is already at a min.
-        # For this, the gradient vector needs to be a zero vector (within tolerance)
+        # For this, the norm of gradient vector needs to be zero (within tolerance)
         gradient_vector = df(guess)
         if (np.linalg.norm(gradient_vector) < TOLERANCE):
             print("Iterations: ", i)
             print("Minimum at: ", np.round(guess, 5))
             print()
             return np.round(guess, 5) # return the guess vector that evaluates to the min of the function 
-        # compute the new guess
-        # if backtracking, reduce step until Wolfe condition is met
+        # if did not find the min, first decide on the step size
+        # if backtracking=True, reduce step by constant factor until Wolfe condition is met
         if (backtracking):
             step = alpha
-            # shring step until Wolfe condition is met
+            # shrink step until Wolfe condition is met
             while (f(guess - step * gradient_vector) > (f(guess) - c*step* (gradient_vector@gradient_vector))):
                 step = RHO * step
         # else if not backtracking but alpha is given as a constant step, use it directly
@@ -59,17 +62,21 @@ def gradient_descent(f, df, guess, alpha, backtracking = False):
             step = alpha
         # if alpha is a function, compute the optimal step
         else:
-            step = alpha(gradient_vector)/10 #smth is wrong with the step calculation here. it is too big. if dividing step by 10, it converges. 
+            step = alpha(gradient_vector) 
             # print("Step: ", step) # debugging
+        # after determining the step size, calculate the new guess
         guess1 = guess - step * gradient_vector 
         # print("New guess", guess1) # debugging
+        # stopping condition: if difference between guesses is negligible
         if (np.linalg.norm(guess1-guess) < TOLERANCE):
             print("Difference between guesses is negligible")
             print("Iterations: ", i)
             print("Minimum at: ", np.round(guess1, 5))
             print()
             return np.round(guess1, 5)
+        # update guess, repeat loop
         guess = guess1
+    # if reached max iterations:
     print("MAX ITERATIONS REACHED")
     print("Closest approximation of the minimum at: ", guess)
     print()
@@ -90,7 +97,6 @@ def f2(vec):
     x2 = vec[1]
     return (10**6) * x1*x1 + x2*x2
 
-
 def f3(vec):
     x1 = vec[0]
     x2 = vec[1]
@@ -99,10 +105,10 @@ def f3(vec):
     x5 = vec[4]
     return x1*x1 + x2*x2 + x3*x3 + x4*x4 + x5*x5 
 
-
 def f4(vec):
     return 0.5 * ((vec@A)@vec) - b@vec
-    
+
+# # Debugging 
 # print("CHECK f4")
 # print(f4(np.array([-1,0,1])))  # works correctly
 
@@ -136,6 +142,7 @@ def gradf4(vec):
     # print(gradient)
     return gradient
 
+# # Debugging
 # print("CHECK gradient f4")
 # gradf4(np.array([-1,0,1]))
 # gradf4(np.array([1,0,1])) # works correctly
@@ -145,11 +152,12 @@ def gradf4(vec):
 
 
 '''
-calculating optimal step size for certain objective functions
+calculating optimal step size for certain objective functions (f4)
 '''
 def get_alpha(gradient):
     return (gradient@gradient)/((gradient@A)@gradient)
 
+# # Debugging
 # print("Check get_alpha")
 # print(get_alpha(np.array([1,2,3]))) # works correctly
 
@@ -164,24 +172,26 @@ if __name__ == "__main__":
     print("Function 1")
     
     myguess = np.array([1.0, 1.0])
-    gradient_descent(f1, gradf1, myguess, .9)
+    gradient_descent(f1, gradf1, myguess, .9) # large step, slow convergence
     print()
     print()
-    gradient_descent(f1, gradf1, myguess, .1)
+    gradient_descent(f1, gradf1, myguess, .1) # small step, also slow 
     print()
     print()
-    gradient_descent(f1, gradf1, myguess, .4)
+    gradient_descent(f1, gradf1, myguess, .4) # much better middle ground, only 15 iterations
     print()
     print()
-    gradient_descent(f1, gradf1, myguess, .5)
+    gradient_descent(f1, gradf1, myguess, .5) # single iteration, lucky choice of step
     print()
     print()
 
-    # print("Function 2")
-    # myguess1 = np.array([2.0, 1.0])
-    # # gradient_descent(f2, gradf2, myguess1, 0.9) #overflow, need to scale??
-    # # gradient_descent(f2, gradf2, myguess1, 0.0000001)
-    ''' Does not converge well. If we choose a big constant step, 
+
+
+    print("Function 2")
+    myguess1 = np.array([2.0, 1.0])
+    gradient_descent(f2, gradf2, myguess1, 0.9) # overflow, diverges
+    gradient_descent(f2, gradf2, myguess1, 0.0000001) # convergence is too slow
+    ''' Either diverges (if step is too big) or converges extremely slowly.
 
     The scaling is so bad that the method with constant step size does
     not work here. Poorly behaved.
@@ -205,10 +215,8 @@ if __name__ == "__main__":
     print()
     print()
     gradient_descent(f4, gradf4, guess4, get_alpha)
-    '''
-    The optimal step seems wrong. function only converges when we divide alpha by about 1.
-    Diverges if alpha is kept according to the formula
-    '''
+    ''' all guesses work correctly, produce [2,2,2] as an answer'''
+
 
 
     # part 3
@@ -217,11 +225,14 @@ if __name__ == "__main__":
 
     print("Function 1")
     gradient_descent(f1, gradf1, myguess, 1.0, backtracking=True)
+    ''' correct here '''
 
     print("Function 2")
     gradient_descent(f2, gradf2, myguess, 1.0, backtracking=True)
+    ''' same issue with poorly scaled issues as with constant step'''
 
     print("Function 3")
     gradient_descent(f3, gradf3, np.array([10,10,10,10,10]), 1.0, backtracking=True)
+    ''' works fine here too'''
 
 
--- hw2p2.py
+++ hw2p2.py
@@ -2,7 +2,7 @@
 Title: Homework 2, part 2. Newton's Method in Multiple Dimensions
 Author: Timur Kasimov (Grinnell, 2025)
 Data Created: February 27, 2024
-Date Updated: February 27, 2024
+Date Updated: March 1st, 2024
 
 Purpose: Implement and test Newton's method in Multiple Dimensions
 
@@ -21,12 +21,25 @@ TOLERANCE = 1e-10
 #####################
 ## NEWTON'S METHOD ##
 #####################
+'''
+Pre-conditions:
+  f: function of n variables
+  gradient df: a function that computes the gradient of f at given coordinates
+  hesssian: a function that takes in a guess vector and computes hessian matrix at that position
+  guess: vector of size n with initial guess coordinates
+Post-conditions: 
+  returns list of vectors of size n with all the guesses
+  prints the best guess for the minimum of the function f
+'''
 def newton(f, df, hessian, guess):
+    # initialize a list ot record all guesses that will be returned
     guesses = []
+    # append initial guess
     guesses.append(guess)
-    for i in range(1, MAXITER+1):
+    # run loop until reached max iterations
+    for i in range(0, MAXITER):
         # Check if the current guess is already at a min.
-        # For this, the gradient vector needs to be a zero vector (within tolerance)
+        # For this, the norm of the gradient vector needs to be zero (within tolerance)
         gradient_vector = df(guess)
         if (np.linalg.norm(gradient_vector) < TOLERANCE):
             print("Iterations: ", i)
@@ -34,8 +47,9 @@ def newton(f, df, hessian, guess):
             print()
             return guesses # return the guess vector that evaluates to the min of the function 
         # compute the new guess, update
-        guess1 = guess - np.linalg.inv(hessian) @ gradient_vector
-        guesses.append(guess1)
+        guess1 = guess - np.linalg.inv(hessian(guess)) @ gradient_vector
+        #append the new guess right awayy
+        guesses.append(guess1) 
         #if the new guess is not different from prev guess (within tolerance) , then terminate
         if (np.linalg.norm(guess1-guess) < TOLERANCE):
             print ("Iterations:", i)
@@ -44,6 +58,7 @@ def newton(f, df, hessian, guess):
         else:
             # update the current guess, iterate again
             guess = guess1
+    # stopping condition: reached max num of iterations
     print("REACHED MAXIMUM NUMBER OF ITERATIONS")
     print("The closest approximation of zero is at x=", round(guess, 6))
     return guesses
@@ -62,7 +77,6 @@ def f2(vec):
     x2 = vec[1]
     return (10**6) * x1*x1 + x2*x2
 
-
 def f3(vec):
     x1 = vec[0]
     x2 = vec[1]
@@ -70,6 +84,12 @@ def f3(vec):
     x4 = vec[3]
     x5 = vec[4]
     return x1*x1 + x2*x2 + x3*x3 + x4*x4 + x5*x5 
+
+def f4(vec):
+    x1 = vec[0]
+    x2 = vec[2]
+    return math.cos(x1) + math.sin(x2)
+
 
 
 ########################
@@ -94,25 +114,36 @@ def gradf3(vec):
     x5 = vec[4]
     return np.array([2*x1, 2*x2, 2*x3, 2*x4, 2*x5])
 
+def gradf4(vec):
+    x1 = vec[0]
+    x2 = vec[1]
+    return np.array([-1 * math.sin(x1), math.cos(x2)])
+
 
 
 ########################
 ###     HESSIANS     ###
 ########################
 
-Hf1 = np.array([2,0,0,2], dtype="float").reshape(2,2)
+def Hf1(vec): 
+    return np.array([2,0,0,2], dtype="float").reshape(2,2)
 
-Hf2 = np.array([2*(10**6),0,0,2], dtype="float").reshape(2,2)
+def Hf2(vec):
+    return np.array([2*(10**6),0,0,2], dtype="float").reshape(2,2)
 
-Hf3 = np.array([[2,0,0,0,0],
-                [0,2,0,0,0],
-                [0,0,2,0,0],
-                [0,0,0,2,0],
-                [0,0,0,0,2]], dtype="float")
+def Hf3(vec):
+    return np.array([[2,0,0,0,0],
+                     [0,2,0,0,0],
+                     [0,0,2,0,0],
+                     [0,0,0,2,0],
+                     [0,0,0,0,2]], dtype="float")
 
-# print(Hf1)
-# print(Hf2)
-# print(Hf3)
+def Hf4(vec):
+    x1 = vec[0]
+    x2 = vec[1]
+    return np.array([[-1 * math.cos(x1), 0],
+                     [0, -1 * math.sin(x2)]], dtype="float")
+
 
 
 
@@ -142,27 +173,41 @@ if __name__ == "__main__":
     newton(f2, gradf2, Hf2, np.array([100.0, 150.0]))
     
     print("Function 3")
-    newton_guesses_3 = newton(f3, gradf3, Hf3, np.array([100.0, 150.0, -94.0, 17.234, 0.0]))
+    newton(f3, gradf3, Hf3, np.array([100.0, 150.0, -94.0, 17.234, 0.0]))
+    print()
+
+    print("Function 4")
+    newton_guesses_4 = newton(f4, gradf4, Hf4, np.array([3.0, 4.0]))
     print()
     print()
 
 
     #Part 2
     '''
-    Newton method seemingly only takes 2 iterations, or exactly one update.
+    Newton method seemingly only takes 1 iteration, or exactly one update.
     This is significantly smaller than the number of iterations that
-    gradient descent takes, specifically between 2 and 109 for function 1.
+    gradient descent takes, specifically between 1 and 108 for function 1,
+    depending on how good the initial guess is.
 
     Gradient descent does not converge for function 2 due to scaling, 
     or converges extremely slowly if we take small enough steps. However,
     newton's method converges in 2 iterations for function 2, so it does not face
     issues with poorly scaled problems.
+
+
+    UPDATE:
+    once we have a more complicated function, like f4 above, newton method takes more iterations 
+    to converge, since our derivatives are no longer linear. 
+    Specifically, function 4 takes 4 iterations to converge when the initial guess is [3,4]
      '''
     
 
 
     # Part 3
     print("Part 3")
-    print(newton_guesses_3)
-    plot_errors(newton_guesses_3, np.array([0,0,0,0,0]), "Convergence Rate")
-
+    print(newton_guesses_4)
+    plot_errors(newton_guesses_4, np.array([math.pi, 3*math.pi/2]), "Convergence Rate")
+    ''' 
+    The plot definitely has at least quadratic convergence, but perhaps even better?
+    The slope to me looks like it is closer to 3 than it is to 2.
+    '''
